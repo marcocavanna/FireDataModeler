fda3c6cd4f0469a383235b5062237ce2346d2a07
Marco Cavanna <info@marcocavanna.com>
2018-11-21 07:48:58 +0100

    Styled Init

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 /**
- * Init WeakMap to mantain certain property
- * private and not accessible from outside
+ * @mooxed/fire-data-modeler 1.3.0 ( 2018-11-21 )
+ * https://github.com/marcocavanna/FireDataModeler
+ * 
  */
-global._dataModeler = new WeakMap();
-global._firebaseAdmin = new WeakMap();
-global._pathReplacers = new WeakMap();
-global._bindings = new WeakMap();
-global._models = new WeakMap();
-global._functions = new WeakMap();
-global._filters = new WeakMap();
+const FireDataModeler = require('./class/fire.data.modeler');
 
-/**
- * Export the Module
- */
-module.exports = require('./class/fire.data.modeler');
+(function moduleInit() {
+
+  /**
+   * Init WeakMap to mantain certain property
+   * private and not accessible from outside
+   */
+  global._dataModeler = new WeakMap();
+  global._firebaseAdmin = new WeakMap();
+  global._pathReplacers = new WeakMap();
+  global._models = new WeakMap();
+  global._functions = new WeakMap();
+  global._filters = new WeakMap();
+  global._cache = new WeakMap();
+  
+  /**
+   * Export the Module
+   */
+  module.exports = FireDataModeler;
+
+}).call(this);
